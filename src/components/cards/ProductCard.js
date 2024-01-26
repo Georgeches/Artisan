6d85fb161064c2813e926347ccf8369c2b52ee9f
Georgeches <georgechesire83@gmail.com>
2024-01-26 10:42:41 +0300

    create shop page

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -2,8 +2,13 @@ export default function ProductCard({page}){
 
     const link = '/products/x';
 
+    const productClass = page==='shop'? 
+        "card shop-product-card border-0 w-100 mt-4"
+        :
+        "card product-card border-0 w-100 mt-4 me-2"
+
     return(
-        <div className="card product-card border-0 mt-4 me-3">
+        <div className={productClass}>
             <div className="card-image">
                 <a href={link}><img src="https://imgs.search.brave.com/RVeccZOntqIKVuL8DkBNopO4NhY1YzFFPyB0ocnaDXg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMzLm5vdmljYS5u/ZXQvMjAxNS9hc3Nl/dHMvaW1hZ2VzL2Nh/dGVnb3J5cGxhdGUv/cGFpbnRpbmdzL2lt/cHJlc3Npb25pc3Rf/MjAyMU1BUjMxLmpw/Zw" alt="category"/></a>
             </div>
---
+++ src/components/pages/Shop.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ProductCard from "../cards/ProductCard";
+import './css/shop.css'
+
+export default function Shop({artisans}){
+
+    return(
+        <div className="container-fluid px-lg-4" style={{marginTop: "80px"}}>
+            <div className="row">
+                <div className="col-md-3">
+                    <p className="h4">Filters</p>
+                </div>
+
+                <div className="col-md-9">
+                    <form className="" style={{position: "relative", width: "80%"}}>
+                        <input className="form-control" type="text" placeholder="Search..."/>
+                        <button className='btn btn-link search-icon' style={{top: '0px', right: '1vh', fontSize: '17px'}}>
+                            <i class="bi bi-search"></i>
+                        </button>
+                    </form>
+
+                    <div className=" mt-3">
+                        <p className="h5">Shop</p>
+
+                        <div className="d-flex justify-content-start gap-2 flex-wrap">
+                            <div className="result">
+                                <ProductCard page={'shop'}/>
+                            </div>
+
+                            <div className="result">
+                                <ProductCard page={'shop'}/>
+                            </div>
+
+                            <div className="result">
+                                <ProductCard page={'shop'}/>
+                            </div>
+
+                            <div className="result">
+                                <ProductCard page={'shop'}/>
+                            </div>
+
+                            <div className="result">
+                                <ProductCard page={'shop'}/>
+                            </div>
+
+                            <div className="result">
+                                <ProductCard page={'shop'}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
